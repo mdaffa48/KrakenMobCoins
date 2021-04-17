b1b4583252bf5cf68aee393b3c7afcd19b47d28f
mdaffa48 <xirpl1.muhammaddaffa@gmail.com>
2021-04-17 17:32:24 +0700

    Updated API

--- src/main/java/me/aglerr/krakenmobcoins/api/MobCoinsAPI.java
+++ src/main/java/me/aglerr/krakenmobcoins/api/MobCoinsAPI.java
@@ -3,21 +3,28 @@ package me.aglerr.krakenmobcoins.api;
 import me.aglerr.krakenmobcoins.MobCoins;
 import me.aglerr.krakenmobcoins.database.PlayerCoins;
 import me.aglerr.krakenmobcoins.manager.AccountManager;
+import me.aglerr.krakenmobcoins.manager.RotatingShopTimeManager;
+import me.aglerr.krakenmobcoins.manager.SalaryManager;
+import me.aglerr.krakenmobcoins.manager.ToggleNotificationManager;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 
 public class MobCoinsAPI {
     
     private final MobCoins plugin;
-    private final AccountManager accountManager;
     public MobCoinsAPI(final MobCoins plugin){
         this.plugin = plugin;
-        this.accountManager = plugin.getAccountManager();
     }
 
     @Nullable
     public PlayerCoins getPlayerData(Player player){
-        return accountManager.getPlayerData(player.getUniqueId().toString());
+        return plugin.getAccountManager().getPlayerData(player.getUniqueId().toString());
     }
+
+    public ToggleNotificationManager getNotificationManager() { return plugin.getNotificationManager(); }
+
+    public SalaryManager getSalaryManager() { return plugin.getSalaryManager(); }
+
+    public RotatingShopTimeManager getRotatingShopTimeManager() { return plugin.getTimeManager(); }
 
 }
